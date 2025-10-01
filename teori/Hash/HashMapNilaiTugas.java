9854b3c1806b8294d9267b304ef2f52f0d9f0e4b
rmhyps1 <240605110123@student.uin-malang.ac.id>
2025-10-01 07:08:40 +0700

    hash1

---
+++ teori/Hash/HashMapNilaiTugas.java
@@ -0,0 +1,28 @@
+package Hash;
+
+import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class HashMapNilaiTugas {
+    public static void main(String[] args) {
+        HashMap<String, ArrayList<Double>> nilaiTugasMap = new HashMap();
+
+        ArrayList<Double> nilaiTugas1 = new ArrayList<>();
+        nilaiTugas1.add(85.0);
+        nilaiTugas1.add(90.0);
+        nilaiTugas1.add(78.0);
+        nilaiTugasMap.put("1801", nilaiTugas1);
+
+        ArrayList<Double> nilaiTugas2 = new ArrayList<>();
+        nilaiTugas2.add(88.0);
+        nilaiTugas2.add(92.0);
+        nilaiTugas2.add(80.0);
+        nilaiTugasMap.put("1802", nilaiTugas2);
+
+        for (String nim: nilaiTugasMap.keySet()) {
+            System.out.println("NIM: " + nim);
+            System.out.println("Nilai Tugas: " + nilaiTugasMap.get(nim));
+            System.out.println();
+        }
+}
---
+++ teori/Hash/HashMapT.java
@@ -0,0 +1,59 @@
+package Hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapT {
+    public static void main(String[] args) {
+        //Deklarasi dan Inisialisasi HashMap
+        //Key: String (NIM), Value: String (Nama)
+        Map<String, String> studentMap = new HashMap<>();
+
+        //Menambahkan elemen menggunakan method put()
+        System.out.println("--- 1. Menambahkan Elemen (put) ---");
+        studentMap.put("1801", "Ani Susanti");
+        studentMap.put("1802", "Budi Darmawan");
+        studentMap.put("1803", "Citra Dewi");
+        studentMap.put("1804", "Deni Pratama");
+        System.out.println("HashMap setelah penambahan elemen: " + studentMap);
+
+        //mengambil nilai berdasarkan kunci mengggunakan method get()
+        System.out.println("\n--- 2. Mengambil Nilai (get) ---");
+        String studentName = studentMap.get("1802");
+        System.out.println("Nama mahasiswa dengan NIM 1802: " + studentName);
+
+        //Mengubah Nilai (menimpa) untuk kunci yang sudah ada (put)
+        System.out.println("\n--- 3. Mengubah Nilai (put) ---");
+        studentMap.put("1803", "Citra Kusuma"); // Mengubah nama untuk NIM 1803
+        System.out.println("HashMap setelah mengubah elemen: " + studentMap);
+
+        //menghapus pasangan kunci-nilai menggunakan method remove()
+        System.out.println("\n--- 4. Menghapus Elemen (remove) ---");
+        studentMap.remove("1804"); 
+        System.out.println("HashMap setelah menghapus 1804: " + studentMap);
+
+        //mengecek kunci dan nilai menggunakan method containsKey() dan containsValue()
+        System.out.println("\n--- 5. Mengecek Kunci dan Nilai (containsKey, containsValue) ---");
+        boolean hasKey = studentMap.containsKey("1801");
+        System.out.println("Apakah ada NIM 1801? " + hasKey);
+
+        boolean hasValue = studentMap.containsValue("Budi Darmawan");
+        System.out.println("Apakah ada nama 'Budi Darmawan'? " + hasValue);
+
+        //Irerasi (perulangan) melalui HashMap menggunakan entrySet()
+        System.out.println("\n--- 6. Iterasi Melalui HashMap (entrySet) ---");
+        for (Map.Entry<String, String> entry : studentMap.entrySet()) {
+            System.out.println("NIM: " + entry.getKey() + ", Nama: " + entry.getValue());
+        }
+
+        //Mendapatkan ukuran HashMap
+        System.out.println("\n--- 7. Ukuran ---");
+        System.out.println("Jumlah Mahasiswa saat ini: " + studentMap.size());
+
+        //Membersihkan seluruh isi HashMap
+        System.out.println("\n--- 8. Membersihkan HashMap (clear) ---");
+        studentMap.clear();
+        System.out.println("HashMap setelah clear: " + studentMap);
+        System.out.println("Apakah HashMap kosong? " + studentMap.isEmpty());
+    }
+}
