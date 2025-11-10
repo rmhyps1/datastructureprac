4b32eecbd8a3febb2996c51395a8d1f12a0adf80
Hanif Yasir <240605110123@student.uin-malang.ac.id>
2025-11-10 15:14:02 +0000

    ril

---
+++ m10/pendahuluan/GraphApp1.java
@@ -0,0 +1,25 @@
+package m10.pendahuluan;
+
+public class GraphApp1 {
+    public static void main(String[] args) {
+        Graph theGraph = new Graph();
+
+        theGraph.addVertex('A');
+        theGraph.addVertex('B');
+        theGraph.addVertex('C');
+        theGraph.addVertex('D');
+        theGraph.addVertex('E');
+
+        theGraph.addEdge(0, 1);
+        theGraph.addEdge(1, 2);
+        theGraph.addEdge(0, 3);
+        theGraph.addEdge(3, 4);
+        theGraph.display();
+
+        //theGraph.dfs();
+        //System.out.println("");
+        //theGraph.bfs();
+        //System.out.println("");
+        theGraph.mst();
+    }
+}
---
+++ m10/pendahuluan/Vertex.java
@@ -0,0 +1,11 @@
+package m10.pendahuluan;
+
+public class Vertex {
+    public char label;
+    public boolean wasVisited;
+
+    public Vertex(char label) {
+        this.label = label;
+        this.wasVisited = false;
+    }
+}
